c0deb6c9ab4ddaab4a5829e198acaaf2e2a8e5ee
wagner <wagnersanches5@hotmail.com>
2023-10-21 01:56:08 -0300

    adicionando as imagens no repositorio

---
+++ assets/images/Character_006_zqimLC.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Character_006_zqimLC" tilewidth="32" tileheight="32" spacing="8" tilecount="25" columns="5">
+ <image source="../../../AppData/Local/Temp/Character_006_zqimLC" width="192" height="192"/>
+</tileset>
---
+++ assets/images/FullSpritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="FullSpritesheet" tilewidth="24" tileheight="24" tilecount="180" columns="12">
+ <image source="FullSpritesheet.png" width="288" height="360"/>
+</tileset>
